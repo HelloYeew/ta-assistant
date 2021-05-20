da78cf2e3cbeb70734c3ad8ea5e5ba5f524b4ad8
Phawit Pornwattanakul <me@helloyeew.dev>
2021-05-20 16:54:19 +0700

    Add grader status example

--- assets/js/work.js
+++ assets/js/work.js
@@ -7,3 +7,13 @@ $('#student2-list a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
 })
+
+$('#student3-list a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+
+$('#student4-list a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
