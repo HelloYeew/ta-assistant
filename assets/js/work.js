5bec1b58e0b6050d5579736168869c82385410f7
Phawit Pornwattanakul <me@helloyeew.dev>
2021-05-20 15:28:33 +0700

    Add multiple tab on student list

---
+++ assets/js/work.js
@@ -0,0 +1,9 @@
+$('#student1-list a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+
+$('#student2-list a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
